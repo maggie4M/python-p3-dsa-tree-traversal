508833047a9727429cbfd0b66349fb680e2bdaec
Margaret <mungaimargaretn@gmail.com>
2024-06-10 11:50:01 +0300

    Completed Assignment

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,37 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      current = nodes_to_visit.pop()
+      if current['id'] == id:
+        return current
+
+      nodes_to_visit = current['children'] + nodes_to_visit
+    return None
+    
+  
+  def breadth_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      result.append(node['value'])
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    return result
+  
+  
+
+  def depth_first_traversal(node):
+    result = []
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      result.append(node['value'])
+      nodes_to_visit = node['children'] + nodes_to_visit
+
+    return result
